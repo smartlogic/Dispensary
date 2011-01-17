8365ade4b8ea6e6cdb2ee4dec17d9c4569711937
Ed Schmalzle <Jonas714@gmail.com>
2011-01-17 11:26:37 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dispensary::Application.config.secret_token = 'ce79cc15b2cf3341cf499389f853d1fd17f0480158b284a07fb4bf8e49c8ee77f7737d8483cc7e61baf91fb140bc8d1114521b9d1b95296ab949dfffd17c3ca2'
